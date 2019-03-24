f2c536a1f67c55064ba768ba273da37a266cb278
Anindita <dasgupta_anindita@ymail.com>
2019-03-24 16:27:49 +0530

    Delete game1.sql

--- game1.sql
+++
@@ -1,9 +0,0 @@
-create database GAME;
-use GAME;
-create table FACT (F_ID varchar(20) primary key, fact varchar(100));
-show tables;
-describe FACT;
-create table LEVEL1 (LEV_ID varchar(20) primary key, fromi int(10), toi int(10),leveli int(10),time1 int(10)); 
-show tables;
-describe LEVEL1;
-
